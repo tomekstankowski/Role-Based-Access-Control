22c5ab8d501544edcd640c99c08ecaf9daa81714
tomekStankowski <t.stankowski@peersoft.pl>
2018-04-24 22:50:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample application presenting **Discretionary Access Control**
---
+++ src/main/java/com/stankowski_strzelka/dac/web_security/WebSecurity.java
@@ -0,0 +1,15 @@
+package com.stankowski_strzelka.dac.web_security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+}
